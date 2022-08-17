61c91e6380305575eb7bd96bc989b1f26b5dc685
ki-hong <rlghd153@gmail.com>
2022-08-17 13:38:23 +0900

    Initial commit

---
+++ src/main/java/com/codestates/soloprojectkihong/SoloProjectKihongApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.soloprojectkihong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloProjectKihongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoloProjectKihongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codestates/soloprojectkihong/SoloProjectKihongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.soloprojectkihong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoloProjectKihongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
